fc415ebb5aa85d50e21f961268d0e692f4f6ecc4
Darkfield <lukaszzz0916@gmail.com>
2019-10-16 20:55:59 +0300

    Add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Test',
+            'email' => 'test1@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Test2',
+            'email' => 'test2@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('transfers')->insert(
+            ['receiver_id' => 1, 'sender_id' => 2, 'amount' => 100]
+        );
+
+        DB::table('transfers')->insert(
+            ['receiver_id' => 2, 'sender_id' => 1, 'amount' => 200]
+        );
     }
 }
